ebec73706da015b44b2de931ac1d9870ce6ede17
jawikiaz <jawikiaz@gmail.com>
2016-12-24 14:56:55 +0800

    m

---
+++ tipuesearch/tipuesearch_content.js
@@ -0,0 +1,13 @@
+
+var tipuesearch = {"pages": [
+	{
+		"title": "Article | Who is Jay C. Angue?",
+		"text": "Know more about Jay C. Angue",
+		"tags": "articleaboutjaycangue",
+		"url": "../files-articles/a-whoisjaycangue.html"
+	},
+	
+]};
+
+
+
